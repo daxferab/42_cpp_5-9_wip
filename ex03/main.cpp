ffe414fb79cbe6088f0ca5eb81738971b6ce97b4
daxferab <daxferab.19@gmail.com>
2025-12-25 04:51:12 +0100

    Add: cpp05:ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -3,76 +3,36 @@
 #include "include/ShrubberyCreationForm.hpp"
 #include "include/RobotomyRequestForm.hpp"
 #include "include/PresidentialPardonForm.hpp"
+#include "include/Intern.hpp"
 
 int main(void)
 {
-	ShrubberyCreationForm	shrub("Sahara");
-	RobotomyRequestForm		robo("Rudolf");
-	PresidentialPardonForm	presi("Frank");
-	Bureaucrat				god("God", 1);
-	Bureaucrat				human("Human", 150);
-	srand(time(NULL));
+	Intern	dummy;
 
 	std::cout << std::endl;
-	std::cout << "Created:" << std::endl;
-	std::cout << god << std::endl;
+	std::cout << "-----------Creating wrong name form-----------" << std::endl;
 	std::cout << std::endl;
-	std::cout << "Trying to execute unsigned form..." << std::endl;
+	AForm	*wrong = dummy.makeForm("wrong name", "random");
+	(void)wrong;
+	std::cout << std::endl;
+	std::cout << "-----------Creating shrubbery form-----------" << std::endl;
+	std::cout << std::endl;
+	AForm	*shrub = dummy.makeForm("shrubbery creation", "random");
+	std::cout << "--Created form: " << shrub->getName() << std::endl;
+	std::cout << std::endl;
+	std::cout << "-----------Creating robotomy form-----------" << std::endl;
+	std::cout << std::endl;
+	AForm	*robo = dummy.makeForm("robotomy request", "random");
+	std::cout << "--Created form: " << robo->getName() << std::endl;
+	std::cout << std::endl;
+	std::cout << "-----------Creating presidential form-----------" << std::endl;
+	std::cout << std::endl;
+	AForm	*presi = dummy.makeForm("presidential pardon", "random");
+	std::cout << "--Created form: " << presi->getName() << std::endl;
+	std::cout << std::endl;
 
-	try
-	{
-		shrub.execute(god);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Couldn't execute because " << e.what() << '\n';
-	}
-	god.executeForm(robo);
-	human.executeForm(robo);
-	std::cout << std::endl;
-	std::cout << "Signing..." << std::endl;
-	god.signForm(shrub);
-	god.signForm(robo);
-	god.signForm(presi);
-	std::cout << std::endl;
-	std::cout << "Executing forms..." << std::endl;
-	god.executeForm(robo);
-	human.executeForm(robo);
-	try
-	{
-		shrub.execute(god);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Couldn't execute because " << e.what() << '\n';
-	}
-	std::cout << std::endl;
-	try
-	{
-		robo.execute(god);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Couldn't execute because " << e.what() << '\n';
-	}
-	std::cout << std::endl;
-	try
-	{
-		presi.execute(god);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Couldn't execute because " << e.what() << '\n';
-	}
-	std::cout << std::endl;
-	std::cout << "Trying to execute without permissions..." << std::endl;
-	try
-	{
-		presi.execute(human);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Couldn't execute because " << e.what() << '\n';
-	}
-	std::cout << std::endl;
+	delete wrong;
+	delete shrub;
+	delete robo;
+	delete presi;
 }
