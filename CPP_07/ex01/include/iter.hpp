00925ff796240e198a2e5cd2448ce863f0556f1f
daxferab <daxferab.19@gmail.com>
2025-12-29 18:02:57 +0100

    Add: cpp07.ex01

--- CPP_07/ex01/include/iter.hpp
+++ CPP_07/ex01/include/iter.hpp
@@ -1,10 +1,23 @@
 #ifndef ITER_HPP
 #define ITER_HPP
+#include <stddef.h>
+#include <iostream>
 
-template <typename T, typename L, typename F>
-void	iter(T array, const L lenght, F function)
+template <typename T, typename Func>
+void	iter(T* array, const size_t lenght, Func func)
 {
+	std::cout << "Applying function to array elements..." << std::endl;
 
+	for (size_t i = 0; i < lenght; i++)
+	{
+		func(array[i]);
+	}
+}
+
+template <typename T>
+void	increment(T& element)
+{
+	element++;
 }
 
 #endif
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -1,6 +1,52 @@
 #include "include/iter.hpp"
+#include <iostream>
 
 int main ()
 {
+	int				arrayInts[] = {1, 2, 3, 4};
+	char			arrayChar[] = {'a', 'b', 'c', 'd'};
+	double			arrayDouble[] = {1.5, 0.6, 3.5, 7.4};
+	size_t const	length = 4;
 
+	std::cout << "Original Ints Array: {";
+	for (size_t i = 0; i < length; i++)
+		std::cout << arrayInts[i] << (i < (length - 1) ? ", " : "");
+	std::cout << "}" << std::endl;
+
+	iter(arrayInts, length, increment<int>);
+
+	std::cout << "Incremented Ints Array: {";
+	for (size_t i = 0; i < length; i++)
+		std::cout << arrayInts[i] << (i < (length - 1) ? ", " : "");
+	std::cout << "}" << std::endl;
+
+	std::cout << "---------------------------------------" << std::endl;
+
+	std::cout << "Original Char Array: {";
+	for (size_t i = 0; i < length; i++)
+		std::cout << arrayChar[i] << (i < (length - 1) ? ", " : "");
+	std::cout << "}" << std::endl;
+
+	iter(arrayChar, length, increment<char>);
+
+	std::cout << "Incremented Char Array: {";
+	for (size_t i = 0; i < length; i++)
+		std::cout << arrayChar[i] << (i < (length - 1) ? ", " : "");
+	std::cout << "}" << std::endl;
+
+	std::cout << "---------------------------------------" << std::endl;
+
+	std::cout << "Original Double Array: {";
+	for (size_t i = 0; i < length; i++)
+		std::cout << arrayDouble[i] << (i < (length - 1) ? ", " : "");
+	std::cout << "}" << std::endl;
+
+	iter(arrayDouble, length, increment<double>);
+
+	std::cout << "Incremented Double Array: {";
+	for (size_t i = 0; i < length; i++)
+		std::cout << arrayDouble[i] << (i < (length - 1) ? ", " : "");
+	std::cout << "}" << std::endl;
+
+	return 0;
 }
